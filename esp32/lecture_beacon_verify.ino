06f8b1ea7f49d8eaf961dc26d5b7373d26426e5b
PROJECT <MAZEN.ALMONAJED.2006@GMAIL.COM>
2026-07-04 23:45:48 +0300

    Simplify attendance flow to WiFi gate only

--- esp32/lecture_beacon_verify.ino
+++ esp32/lecture_beacon_verify.ino
@@ -1,24 +1,12 @@
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
 #include <WiFi.h>
 #include <WebServer.h>
 #include "mbedtls/md.h"
 
-const char* BLE_DEVICE_NAME = "LECTURE_BEACON";
 const char* AP_SSID = "LECTURE_BEACON";
 const char* AP_PASS = "12345678";
 const char* GATE_SECRET = "CHANGE_ME_GATE_SECRET";
 
-const char* BLE_SERVICE_UUID = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
-const char* BLE_LECTURE_CHAR_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
-const char* BLE_TOKEN_CHAR_UUID = "0f3f2e60-8e92-4ea7-9f93-8b4b31c0d9aa";
-
-BLECharacteristic* lectureIdCharacteristic = nullptr;
-BLECharacteristic* tokenCharacteristic = nullptr;
 WebServer webServer(80);
-String lastLectureId;
-String lastIssuedToken;
 
 String hmacSha256Hex(const String& payload, const String& key) {
   unsigned char output[32];
@@ -88,74 +76,20 @@ void handleClaim() {
   webServer.send(302, "text/plain", "Redirecting...");
 }
 
-class LectureIdCallbacks : public BLECharacteristicCallbacks {
-  void onWrite(BLECharacteristic* characteristic) override {
-    auto value = characteristic->getValue();
-    lastLectureId = String(value.c_str());
-    lastLectureId.trim();
-
-    Serial.print("Received lectureId: ");
-    Serial.println(lastLectureId);
-  }
-};
-
-class TokenCallbacks : public BLECharacteristicCallbacks {
-  void onRead(BLECharacteristic* characteristic) override {
-    if (lastLectureId.length() == 0) {
-      characteristic->setValue("ERR:missing_lecture_id");
-      return;
-    }
-
-    lastIssuedToken = buildGateToken(lastLectureId);
-    if (lastIssuedToken.length() == 0) {
-      characteristic->setValue("ERR:token_sign_failed");
-      return;
-    }
-
-    characteristic->setValue(lastIssuedToken.c_str());
-    Serial.println("Issued BLE gate token");
-  }
-};
-
 void setup() {
   Serial.begin(115200);
 
-  WiFi.mode(WIFI_AP_STA);
+  WiFi.mode(WIFI_AP);
   WiFi.softAP(AP_SSID, AP_PASS);
   webServer.on("/claim", HTTP_GET, handleClaim);
   webServer.begin();
 
   Serial.print("AP IP: ");
   Serial.println(WiFi.softAPIP());
-
-  BLEDevice::init(BLE_DEVICE_NAME);
-  BLEServer* server = BLEDevice::createServer();
-  BLEService* service = server->createService(BLE_SERVICE_UUID);
-
-  lectureIdCharacteristic = service->createCharacteristic(
-    BLE_LECTURE_CHAR_UUID,
-    BLECharacteristic::PROPERTY_WRITE
-  );
-  lectureIdCharacteristic->setCallbacks(new LectureIdCallbacks());
-
-  tokenCharacteristic = service->createCharacteristic(
-    BLE_TOKEN_CHAR_UUID,
-    BLECharacteristic::PROPERTY_READ
-  );
-  tokenCharacteristic->setCallbacks(new TokenCallbacks());
-  tokenCharacteristic->setValue("READY");
-
-  service->start();
-
-  BLEAdvertising* advertising = BLEDevice::getAdvertising();
-  advertising->addServiceUUID(BLE_SERVICE_UUID);
-  advertising->setScanResponse(false);
-  BLEDevice::startAdvertising();
-
-  Serial.println("BLE beacon is ready");
+  Serial.println("Wi-Fi gate is ready");
 }
 
 void loop() {
   webServer.handleClient();
-  delay(100);
+  delay(50);
 }
